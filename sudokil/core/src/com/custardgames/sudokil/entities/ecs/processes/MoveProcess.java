4567e0603142eedd52ed00c779fb89266d4691ef
Dominic Mortlock <dom.mortlock@gmail.com>
2016-10-27 16:19:38 -0700

    fixed weird jerk

--- sudokil/core/src/com/custardgames/sudokil/entities/ecs/processes/MoveProcess.java
+++ sudokil/core/src/com/custardgames/sudokil/entities/ecs/processes/MoveProcess.java
@@ -134,8 +134,8 @@ public class MoveProcess extends EntityProcess
 	{
 		UniTweenManager.getTweenManager().killTarget(position, type);
 		Timeline.createSequence()
-				.push(Tween.to(position, type, 1.0f / 4.0f).targetRelative(8 * dir).ease(Expo.OUT))
-				.push(Tween.to(position, type, 1.0f / 2.0f).targetRelative(-8 * dir).ease(Linear.INOUT))
+				.push(Tween.to(position, type, 1.0f / 8.0f).targetRelative(4 * dir).ease(Expo.OUT))
+				.push(Tween.to(position, type, 1.0f / 4.0f).targetRelative(-4 * dir).ease(Linear.INOUT))
 				.start(UniTweenManager.getTweenManager());
 	}
 
